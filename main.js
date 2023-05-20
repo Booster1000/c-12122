f6e83d582f38c52ace6f23212abeb4ec6a66c143
Naitik <104677260+Booster1000@users.noreply.github.com>
2023-05-20 17:25:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function draw(){
+
+}
+
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.position(750,250);
+    camera = createCapture(VIDEO);
+    camera.size(400,400);
+    camera.position(750, 650);
+}
